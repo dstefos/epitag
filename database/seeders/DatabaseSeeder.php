f51893bb9076f49a3ab7a481a354ba0b9ebd52f8
Dimitrios Stefos <dimitrios.stefos@gmail.com>
2021-06-09 22:56:54 +0300

    seed users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Check if there is already an admin and if not then create one
+        $numberOfAdmins=\App\Models\User::where('admin', true)->count();
+        if($numberOfAdmins==0)
+        DB::table('users')->insert([
+            'name' => "Adminios Adminidis",
+            'email' => "admin@epitages.gr",
+            'email_verified_at' => now(),
+            'password' => bcrypt('asdfasdf'), // password
+            'admin' => true,
+            'balance' => 0,
+            'remember_token' => Str::random(10),
+        ]);
+
+        // Create 10 users
+        \App\Models\User::factory(10)->create();
         $this->call([
             CardSeeder::class,
             BundleSeeder::class,
