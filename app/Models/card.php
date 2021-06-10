25ef8e357c86404b9167ee010643f46944a941e0
Dimitrios Stefos <dimitrios.stefos@gmail.com>
2021-06-10 13:27:47 +0300

    add comments

--- app/Models/card.php
+++ app/Models/card.php
@@ -17,14 +17,20 @@ class card extends Model
 
     public function trade(User $buyer)
     {
+        // Check if the card belongs to a user and if the buyer can afford this trade
         if($this->user_id==null || $buyer->balance-$this->price<0)
             return false;
 
+        // Retrieve the owner of the card and increase their balance
         $seller=User::find($this->user_id);
         $seller->balance+=$this->price;
         $seller->save();
+
+        // Decrease the balance of the buyer
         $buyer->balance-=$this->price;
         $buyer->save();
+
+        // Change the owner of the card
         $this->user_id=$buyer->id;
         $this->save();
 
