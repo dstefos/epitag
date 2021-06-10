1e6bb33d32cd835db32a7fef8c64fc7c7e149a8b
Dimitrios Stefos <dimitrios.stefos@gmail.com>
2021-06-10 13:25:30 +0300

    create an instance method in card model, that will be used when a two users trade a card

--- app/Models/card.php
+++ app/Models/card.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class card extends Model
 {
@@ -12,5 +13,22 @@ class card extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function trade(User $buyer)
+    {
+        if($this->user_id==null || $buyer->balance-$this->price<0)
+            return false;
+
+        $seller=User::find($this->user_id);
+        $seller->balance+=$this->price;
+        $seller->save();
+        $buyer->balance-=$this->price;
+        $buyer->save();
+        $this->user_id=$buyer->id;
+        $this->save();
+
+        return true;
+
     }
 }
