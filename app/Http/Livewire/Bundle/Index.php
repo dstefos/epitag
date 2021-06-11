a46f603756b6d9afea094ede8e4153fd14105de6
Dimitrios Stefos <dimitrios.stefos@gmail.com>
2021-06-11 10:45:09 +0300

    build bundle creation form

--- app/Http/Livewire/Bundle/Index.php
+++ app/Http/Livewire/Bundle/Index.php
@@ -5,10 +5,20 @@ namespace App\Http\Livewire\Bundle;
 use App\Models\bundle;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
+use Livewire\WithFileUploads;
 
 class Index extends Component
 {
-    public $bundles;
+    use WithFileUploads;
+
+    public $bundles, $newTitle, $newPrice, $newQuantity, $newImage, $showDialog=false;
+
+    protected $rules = [
+        'newTitle' => 'required',
+        'newQuantity' => 'required',
+        'newPrice' => 'required',
+        'newImage' => 'image',
+    ];
 
     public function mount()
     {
@@ -29,9 +39,34 @@ class Index extends Component
     public function delete(bundle $bundle)
     {
         if(!Auth::user()->admin)
-            return 'not authorized';
+            dd('not authorized');
         
         bundle::destroy($bundle->id);
         $this->bundles=bundle::get();
+        session()->flash('message', 'Bundle deleted.');
+    }
+
+    public function updated()
+    {
+        $this->showDialog=true;
+
+    }
+
+    public function submit()
+    {
+        if(!Auth::user()->admin)
+            dd('not authorized');
+        $this->showDialog=true;
+        $this->validate();
+        // $this->showDialog=false;
+        $this->newImage->store('img', ['disk' => 'public']);
+        // bundle::create(
+        //     [
+        //         'title'=>$this->newTitle,
+        //         'price'=>$this->newPrice,
+        //         'quantity'=>$this->newQuantity,
+        //         'image'=>$this->newImage
+        //     ]
+        // );
     }
 }
--- resources/views/livewire/bundle/index.blade.php
+++ resources/views/livewire/bundle/index.blade.php
@@ -6,14 +6,68 @@
     }
 
     .bundle-box img{
-        width:150px;
+        width:70px;
         margin: 0 auto;
     }
 
     .price b{
         color: green;
     }
+
+    #create-dialog{
+        @if(!$showDialog)display: none;@endif
+        padding: 10px;
+    }
+
 </style>
+@if (session()->has('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+@endif
+<button class="btn btn-primary" onclick="$('#create-dialog').show('slow')">Create Bundle</button>
+<div id="create-dialog">
+    <form wire:submit.prevent="submit">
+        @error('newTitle') <span class="error">{{ $message }}</span> @enderror
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="newTitle">Title</span>
+            </div>
+            <input type="text" class="form-control" placeholder="Title" aria-label="Title" aria-describedby="newTitle" wire:model="newTitle">
+        </div>
+
+        @error('newPrice') <span class="error">{{ $message }}</span> @enderror
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="newPrice">Price</span>
+            </div>
+            <input type="number" step="0.01" class="form-control" placeholder="Price" aria-label="Price" aria-describedby="newPrice" wire:model="newPrice">
+        </div>
+
+        @error('newQuantity') <span class="error">{{ $message }}</span> @enderror
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="newQuantity">Quantity</span>
+            </div>
+            <input type="number" class="form-control" placeholder="Quantity" aria-label="Quantity" aria-describedby="newQuantity" wire:model="newQuantity">
+        </div>
+
+        @error('newImage') <span class="error">{{ $message }}</span> @enderror
+        @if ($newImage)
+            Photo Preview:
+            <img src="{{ $newImage->temporaryUrl() }}">
+        @endif
+        <div class="input-group mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="newImage">Image</span>
+            </div>
+            <input type="file" class="form-control" placeholder="Image" aria-label="Image" aria-describedby="newImage" wire:model="newImage">
+        </div>
+
+        <button class="btn btn-success">Create</button>
+    </form>
+</div>
+<hr>
 @foreach($bundles as $bundle)
     <div class="bundle-box">
         <h4>{{$bundle->title}}</h4>
