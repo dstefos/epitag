4aa76a2dcc698e6ba6748f90713d452442896d68
Dimitrios Stefos <dimitrios.stefos@gmail.com>
2021-06-13 11:05:53 +0300

    create TradeJob model

---
+++ database/factories/TradeJobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TradeJob;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TradeJobFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TradeJob::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_06_13_075337_create_trade_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradeJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trade_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('card_id')->nullable();
+            $table->bigInteger('bundle_id')->nullable();
+            $table->bigInteger('user_id');
+            $table->dateTime('whenTime')->nullable();
+            $table->float('whenPriceBigger')->nullable();
+            $table->float('whenPriceSmaller')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trade_jobs');
+    }
+}
